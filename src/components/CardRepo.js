94a2312a698c30258d94887884609521070d7951
jac <jac.dr1013@gmail.com>
2020-12-09 13:08:08 -0600

    complete card repo view

--- src/components/CardRepo.js
+++ src/components/CardRepo.js
@@ -16,7 +16,7 @@ const AvatarLogo = styled.img `
 /* Data Dummy */
 const dummy = [
     {
-        "id": 78232616,
+        "id": 7823261,
         "name": "simple-line-icons",
         "private": false,
         "owner": {
@@ -32,7 +32,7 @@ const dummy = [
         "default_branch": "master"
     },
     {
-        "id": 78232616,
+        "id": 782326,
         "name": "simple-line-icons",
         "private": false,
         "owner": {
@@ -47,7 +47,7 @@ const dummy = [
         "default_branch": "master"
     },
     {
-        "id": 78232616,
+        "id": 7823,
         "name": "simple-line-icons",
         "private": false,
         "owner": {
@@ -63,36 +63,56 @@ const dummy = [
     },
 ]
 
+const copyClone = (id) => {
+        var copyText = document.getElementById(id);
+        copyText.select();
+        copyText.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+}
 const CardRepo = props => {
     return (
         <Fragment>
             <div className="row">
                 {dummy.map((item) => (
-                    <div className="col-md-6 col-sm-12 p-3">
+                    <div key={item.id} className="col-md-6 col-sm-12 p-3">
                         <div className="card">
-                           <div className="card-body">
-                                <div className="d-flex align-items-center">
-                                    <AvatarContainer>
-                                        <a href={item.owner.url} target="_blank">
-                                            <AvatarLogo className='img-fluid float-start'
-                                                src={item.owner.avatar_url} alt="Avatar" title="Ir a GitHub"/>
+                            <div className="card-body row">
+                                <div className="col-md-6 col-sm-12">
+                                    <div className="d-flex align-items-center">
+                                        <AvatarContainer>
+                                            <a href={item.owner.url} target="_blank">
+                                                <AvatarLogo className='img-fluid float-start'
+                                                    src={item.owner.avatar_url} alt="Avatar" title="Ir a GitHub"/>
+                                            </a>
+                                        </AvatarContainer>
+                                            <a href={item.html_url} target="_blank"
+                                                data-bs-toggle="tooltip"
+                                                data-bs-placement="bottom"
+                                                title="Ir al repositorio">
+                                            <b>{item.name}</b>
                                         </a>
-                                    </AvatarContainer>
-                                        <a href={item.html_url} target="_blank"
+                                    </div>
+                                    <div className="">
+                                        <span>Descripción:</span> <p className="fst-italic">{item.description} </p>
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-sm-12">
+                                    <span>Lenguaje:</span><p className="fst-italic">{item.language}</p>
+                                    <div className="input-group mb-3">
+                                        <input id={item.id} type="text" className="form-control" value={item.clone_url}/>
+                                        <button className="btn btn-outline-secondary" type="button"
                                             data-bs-toggle="tooltip"
                                             data-bs-placement="bottom"
-                                            title="Ir al repositorio">
-                                        <b>{item.name}</b>
-                                    </a>
-                                </div>
-                                <div className="">
-                                    <p className="fst-italic">{item.description} </p>
+                                            title="Copiar por HTTPS "
+                                            onClick={()=>copyClone(item.id)}
+                                        > <i className="fa fa-clone"/></button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 ))}
-
             </div>
         </Fragment>
     )
