a2296683d38208498de0e45708d74bf0c10951c9
jac <jac.dr1013@gmail.com>
2020-12-09 23:34:34 -0600

    set raoter navigation

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import Header from './containers/Header';
 import ListRepo from './containers/ListRepo';
 import ListUsers from './containers/ListUsers';
 
+import { BrowserRouter as Router, Switch, Route  } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { store } from './redux/store'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <NavSearch/>
-      {/* <ListUsers/> */}
-      <ListRepo/>
-    </div>
+    <Router>
+      <Provider store ={store}>
+        <Header/>
+        <NavSearch/>
+        <Switch>
+          <Route exact path='/' />
+          <Route exact path='/user' component={ListUsers}/>
+          <Route exact path='/repo/' component={ListRepo}/>
+        </Switch>
+      </Provider>
+    </Router>
   );
 }
 
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import NavSearch from '../components/NavSearch'
+import { Link } from 'react-router-dom'
 
 function Header(props) {
     return (
         <Fragment>
             <div className=" navbar navbar-light bg-dark shadow py-4">
                 <div className="container">
-                    <img className='img-fluid' src="https://luuna-bucket.imgix.net/img/header-logo.svg?auto=compress,format" alt="LUUNA" title="LUUNA"/>
+                    <Link to="/">
+                        <img className='img-fluid' src="https://luuna-bucket.imgix.net/img/header-logo.svg?auto=compress,format" alt="LUUNA" title="LUUNA"/>
+                    </Link>
                         <h1 className='m-auto text-center text-muted'>
                             Test Luuna
                         </h1>
